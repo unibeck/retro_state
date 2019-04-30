c81394a81868c0da6edef64aed2bf94e9309a4e2
Jonathan Beckman <jonathantbeckman@gmail.com>
2019-04-30 16:59:52 -0400

    Updated consts

--- custom_components/historic_template/const.py
+++ custom_components/historic_template/const.py
@@ -1,11 +1,11 @@
-"""Constants for blueprint."""
+"""Constants for historic_template."""
 # Base component constants
 DOMAIN = "historic_template"
 DOMAIN_DATA = "{}_data".format(DOMAIN)
 VERSION = "1.0.0"
 PLATFORMS = ["sensor"]
 REQUIRED_FILES = ["const.py", "sensor.py", "historic_entity.py"]
-ISSUE_URL = "https://github.com/custom-components/blueprint/issues"
+ISSUE_URL = "https://gitlab.com/jbeckman/retro_state/issues"
 
 STARTUP = """
 -------------------------------------------------------------------
@@ -17,14 +17,8 @@ If you have any issues with this you need to open an issue here:
 -------------------------------------------------------------------
 """
 
-# Operational
-URL = "https://jsonplaceholder.typicode.com/todos/1"
-
 # Icons
-ICON = "mdi:format-quote-close"
-
-# Device classes
-BINARY_SENSOR_DEVICE_CLASS = "connectivity"
+ICON = "mdi:clock-end"
 
 # Configuration
 CONF_SENSOR = "sensor"
